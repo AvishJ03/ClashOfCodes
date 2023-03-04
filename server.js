60d19e0ab86e264282b40f82d9a7bd6723e9c64a
Avish Jain <avishjain0@gmail.com>
2023-03-05 01:44:04 +0530

    socket

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("./db");
 require("dotenv").config();
 const cors = require("cors");
 const app = express();
+const socket = require("socket.io");
 
 const userRouter = require("./routes/user.route");
 const destinationRouter = require("./routes/destination.route");
@@ -17,4 +18,29 @@ app.use(planRouter);
 const port = 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  console.log("Socket");
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-received", data.message);
+    }
+  });
+});
