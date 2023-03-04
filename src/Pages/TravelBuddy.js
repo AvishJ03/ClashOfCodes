45f786a4bd3917d944d46146d523e705d9c3b657
Avish Jain <avishjain0@gmail.com>
2023-03-05 04:04:46 +0530

    changes

--- src/Pages/TravelBuddy.js
+++ src/Pages/TravelBuddy.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import TravelCard from '../Components/TravelCard/TravelCard'
+import React from "react";
+import TravelCard from "../components/TravelCard/TravelCard";
 
 const TravelBudddy = () => {
   return (
     <div>
-        <TravelCard/>
+      <TravelCard />
     </div>
-  )
-}
+  );
+};
 
-export default TravelBudddy
+export default TravelBudddy;
