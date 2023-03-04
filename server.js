e6e48c1e8b67795c40ee48f73354be2790f3c3ec
Avish Jain <avishjain0@gmail.com>
2023-03-04 23:23:12 +0530

    recommend

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ const app = express();
 
 const userRouter = require("./routes/user.route");
 const destinationRouter = require("./routes/destination.route");
+const planRouter = require("./routes/plans.route");
 
 app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(destinationRouter);
+app.use(planRouter);
 
 const port = 5000;
 
